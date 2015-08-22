85017e00d64de65b1806a7255be531d5197248d4
Sourav Singh <ssouravsingh12@gmail.com>
2015-08-22 17:19:51 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions are used to store the value of inverse of a matrix A in S
+## 
 
-## Write a short comment describing this function
+## This function is used to create a cached inverse matrix.We use 4 functions to finally calculate the value of the matrix:
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        S <- NULL
+	set <- function(B)
+	{
+		A <<- B
+		S <<- NULL
+	}
+	get <- function() A
+	setinv <- function(B) S <<- B
+	getinv <- function() S
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
